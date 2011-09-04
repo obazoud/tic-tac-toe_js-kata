760c4da6c5dd1f9b1107aff01c317b58f4ca36b8
Romain <romain.maton@gmail.com>
2011-09-04 16:53:07 +0000

    First online implementation (player and bot) with only one game.

--- WebContent/game.js
+++ WebContent/game.js
@@ -1,47 +1,51 @@
-Game = function() {
-		this.fields = ['_','_','_','_','_','_','_','_','_'];
-        this.curPlayer = "X";
-        this.winner;
-	}
 
-	Game.prototype = {
-       take : function(field) {
-            if (this.fields[field-1]=='_') {
-                this.fields[field-1] = this.curPlayer;
-                 this.curPlayer=this.otherPlayer();
-                return true;
-            }
-            return false;
-        },
-		isFinished : function() {
-    		  var all = this.fields.every(function (field){
-    			return field !="_";
-			    });
+var Game = function() {
+    this.fields = ['_', '_', '_', '_', '_', '_', '_', '_', '_'];
+    this.curPlayer = "X";
+    this.winner;
+}
 
-            var ligne = this.fields[0] + this.fields[1] + this.fields[2] +"|"+this.fields[3] + this.fields[4] + this.fields[5]+"|"+this.fields[6] + this.fields[7] + this.fields[8] ;
-            var colonne = this.fields[0] + this.fields[3] + this.fields[6]+"|"+this.fields[1] + this.fields[4] + this.fields[7]+"|"+this.fields[2] + this.fields[5] + this.fields[8];
-            var diagonale = this.fields[0] + this.fields[4] + this.fields[8]+"|"+this.fields[2] + this.fields[4] + this.fields[6];
-            //check x win
-            if ((ligne+colonne+diagonale).indexOf("XXX") != -1) {
-                this.winner = "X";
-                return true;
-             }else if ((ligne+colonne+diagonale).indexOf("OOO") != -1) {
-                 this.winner = "O";
-                return true;
-              }
-			return all;
-		},
-		currentPlayer : function() {
-			return this.curPlayer;
-		},
-		otherPlayer : function() {
-                if (this.curPlayer=="X"){
-                    return "O";
-                }else{
-                    return "X";
-                }
-            },
-         getFirstPositionAvailable :function(){
-             return this.fields.indexOf('_')+1;
-         }
-	}
+Game.prototype = {
+    take: function(field) {
+        if (this.fields[field - 1] == '_') {
+            this.fields[field - 1] = this.curPlayer;
+            this.curPlayer = this.otherPlayer();
+            return true;
+        }
+        return false;
+    },
+    isFinished: function() {
+        var all = this.fields.every(function(field) {
+            return field != "_";
+        });
+        var ligne = this.fields[0] + this.fields[1] + this.fields[2] + "|" + this.fields[3] + this.fields[4] + this.fields[5] + "|" + this.fields[6] + this.fields[7] + this.fields[8];
+        var colonne = this.fields[0] + this.fields[3] + this.fields[6] + "|" + this.fields[1] + this.fields[4] + this.fields[7] + "|" + this.fields[2] + this.fields[5] + this.fields[8];
+        var diagonale = this.fields[0] + this.fields[4] + this.fields[8] + "|" + this.fields[2] + this.fields[4] + this.fields[6];
+        //check x win
+        if ((ligne + colonne + diagonale).indexOf("XXX") != -1) {
+            this.winner = "X";
+            return true;
+        }
+        else if ((ligne + colonne + diagonale).indexOf("OOO") != -1) {
+            this.winner = "O";
+            return true;
+        }
+        return all;
+    },
+    currentPlayer: function() {
+        return this.curPlayer;
+    },
+    otherPlayer: function() {
+        if (this.curPlayer == "X") {
+            return "O";
+        }
+        else {
+            return "X";
+        }
+    },
+    getFirstPositionAvailable: function() {
+        return this.fields.indexOf('_') + 1;
+    }
+};
+
+exports.Game = Game;
---
+++ WebContent/server.js
@@ -0,0 +1,53 @@
+
+var express = require('express');
+var game = new require('./game');
+
+var server = express.createServer();
+var io = require('socket.io').listen(server);
+
+server.use(express.static(__dirname));
+server.get('/', function(req, res){
+    //res.send('Hello World');
+    res.render('index.html');
+});
+server.listen(process.env.C9_PORT, "0.0.0.0");
+console.log("Server is running, ready for accepting players ^^");
+
+io.sockets.on('connection', function (socket) {
+    round = new game.Game();
+    var winnerMsg = null;
+    var hasWinner = function() {
+        if (round.isFinished() && round.winner !== undefined) {
+            winnerMsg = (round.winner=="X" ? "Player" : "Bot") + " win the Game!";
+            return true;
+        } else if(round.isFinished()) {
+            winnerMsg = "No winner";
+            return true;
+        }
+        return false;
+    };
+    socket.emit('message', { 'message': 'Your turn' });
+    socket.on('try', function (data) {
+        if(round.winner) {
+            socket.emit('message', { type:'error', 'message':'What are you trying to do ;-)' });
+            return;
+        }
+        var isValid = round.take(data.pos);
+        if(!isValid) {
+            socket.emit('message', { type:'error', 'message':'Bad move, play somewhere else' });
+            return;
+        }
+        socket.emit('play', { 'message':'Player plays on '+data.pos, 'pos':data.pos, 'icon':round.otherPlayer() });
+        if(hasWinner()) {
+            socket.emit('message', { type:'winner', 'message': winnerMsg });
+            return;
+        }
+        var firstPositionAvailable = round.getFirstPositionAvailable();
+        round.take(firstPositionAvailable);
+        socket.emit('play', { 'message':'Bot plays on '+firstPositionAvailable, 'pos':firstPositionAvailable, 'icon':round.otherPlayer() });
+        if(hasWinner()) {
+            socket.emit('message', { type:'winner', 'message': winnerMsg });
+            return;
+        }
+    });
+});
