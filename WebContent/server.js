131cac99b5fb2e0f1675f3cb99cdf904750b73ce
Romain <romain.maton@gmail.com>
2011-09-04 16:58:15 +0000

    Set JSON key as String and add some comments.

--- WebContent/server.js
+++ WebContent/server.js
@@ -10,12 +10,13 @@ server.get('/', function(req, res){
     //res.send('Hello World');
     res.render('index.html');
 });
-server.listen(process.env.C9_PORT, "0.0.0.0");
+server.listen(process.env.C9_PORT, "0.0.0.0"); // Cloud9
 console.log("Server is running, ready for accepting players ^^");
 
 io.sockets.on('connection', function (socket) {
     round = new game.Game();
     var winnerMsg = null;
+    
     var hasWinner = function() {
         if (round.isFinished() && round.winner !== undefined) {
             winnerMsg = (round.winner=="X" ? "Player" : "Bot") + " win the Game!";
@@ -26,27 +27,28 @@ io.sockets.on('connection', function (socket) {
         }
         return false;
     };
+    
     socket.emit('message', { 'message': 'Your turn' });
     socket.on('try', function (data) {
         if(round.winner) {
-            socket.emit('message', { type:'error', 'message':'What are you trying to do ;-)' });
+            socket.emit('message', { 'type':'error', 'message':'What are you trying to do ;-)' });
             return;
         }
         var isValid = round.take(data.pos);
         if(!isValid) {
-            socket.emit('message', { type:'error', 'message':'Bad move, play somewhere else' });
+            socket.emit('message', { 'type':'error', 'message':'Bad move, play somewhere else' });
             return;
         }
         socket.emit('play', { 'message':'Player plays on '+data.pos, 'pos':data.pos, 'icon':round.otherPlayer() });
         if(hasWinner()) {
-            socket.emit('message', { type:'winner', 'message': winnerMsg });
+            socket.emit('message', { 'type':'winner', 'message': winnerMsg });
             return;
         }
         var firstPositionAvailable = round.getFirstPositionAvailable();
         round.take(firstPositionAvailable);
         socket.emit('play', { 'message':'Bot plays on '+firstPositionAvailable, 'pos':firstPositionAvailable, 'icon':round.otherPlayer() });
         if(hasWinner()) {
-            socket.emit('message', { type:'winner', 'message': winnerMsg });
+            socket.emit('message', { 'type':'winner', 'message': winnerMsg });
             return;
         }
     });
